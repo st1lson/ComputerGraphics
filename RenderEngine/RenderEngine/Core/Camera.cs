a59177e4117a03daed9cfadd1a22084dcb7e806a
Akara4ok <vladshpilka86@gmail.com>
2023-03-04 17:10:28 +0200

    Add simple renderer

--- RenderEngine/RenderEngine/Core/Camera.cs
+++ RenderEngine/RenderEngine/Core/Camera.cs
@@ -8,14 +8,16 @@ namespace RenderEngine.Core
         public Vector3 Dir { get; init; }
         public int PixelWidth { get; init; }
         public int PixelHeight { get; init; }
+        public int VerticalFOV { get; init; }
         public float FocalLength { get; init; }
-        public Camera(Vector3 orig, Vector3 dir, int pixelWidth, int pixelHeight, float focalLength)
+        public Camera(Vector3 orig, Vector3 dir, int pixelWidth, int pixelHeight, float focalLength, int verticalFOV)
         {
             Orig = orig;
             Dir = dir;
             PixelWidth = pixelWidth;
             PixelHeight = pixelHeight;
             FocalLength = focalLength;
+            VerticalFOV = verticalFOV;
         }
     }
 }
--- RenderEngine/RenderEngine/Core/Renderer.cs
+++ RenderEngine/RenderEngine/Core/Renderer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RenderEngine.Basic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,52 @@ namespace RenderEngine.Core
 
         public void Render()
         {
+            float[,] screen = new float[Camera.PixelHeight, Camera.PixelWidth];
 
+            //pixelDensity = pixels / (absolute size)
+            float pixelDensity = (Camera.PixelHeight / 2) / ((float)Math.Tan(Camera.VerticalFOV) * Camera.FocalLength);
+
+            Vector3 up = new Vector3(0, 0, 1 / pixelDensity);
+            Vector3 right = new Vector3(1 / pixelDensity, 0, 0);
+
+            Vector3 ScreenCenter = Camera.Orig + Camera.Dir * Camera.FocalLength;
+            Vector3 ScreenOrig = ScreenCenter - up * Camera.PixelHeight / 2 - right * Camera.PixelWidth / 2;
+
+            for (int i = 0; i <= Camera.PixelHeight; i++)
+            {
+                for (int j = 0; j < Camera.PixelWidth; j++)
+                {
+                    var sceenPoint = ScreenOrig + up * i + right * j;
+                    var screenCameraRay = new Ray(Camera.Orig, sceenPoint - Camera.Orig);
+                    Vector3? intersectionPoint = null;
+                    float minSquareDistance = float.MaxValue;
+                    foreach (var shape in Scene.Shapes)
+                    {
+                        var intersaction = shape.Intersects(screenCameraRay);
+                        if (intersaction == null) 
+                        {
+                            continue;
+                        }
+
+                        var squareDist = Vector3.Dot(intersaction! - Camera.Orig, intersaction! - Camera.Orig);
+                        if (squareDist < minSquareDistance)
+                        {
+                            intersectionPoint = intersaction;
+                            minSquareDistance = squareDist;
+                        }
+                    }
+
+                    if (intersectionPoint == null) 
+                    {
+                        //TODO: something operation with light mb
+                        screen[i, j] = 0;
+                        continue;
+                    }
+
+                    //TODO: something operation with light mb
+                    screen[i, j] = 1;
+                }
+            }
         }
     }
 }
