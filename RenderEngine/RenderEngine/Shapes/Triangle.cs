df17390c0907d1fb45d53d714f3c16e655911750
Anatolii <Parshykov>
2023-03-06 13:14:03 +0200

    triangle intersection

--- RenderEngine/RenderEngine/Shapes/Triangle.cs
+++ RenderEngine/RenderEngine/Shapes/Triangle.cs
@@ -10,14 +10,49 @@ namespace RenderEngine.Shapes
 {
     public record Triangle : IShape
     {
-        public Point3 v0 { get; init; }
-        public Point3 v1 { get; init; }
-        public Point3 v2 { get; init; }
+        public Vector3 v0 { get; init; }
+        public Vector3 v1 { get; init; }
+        public Vector3 v2 { get; init; }
 
         public Vector3? Intersects(Ray ray)
         {
-            var p = v0 - v1;
-            return null;
+            const double tolerance = 1e-5d;
+
+            var edge1 = v1- v0;
+            var edge2 = v2 - v0;
+
+            var pvec = Vector3.Cross(ray.Dir, edge2);
+
+            var det = Vector3.Dot(edge1, pvec);
+
+            if (det > -tolerance && det < tolerance)
+            {
+                return null;
+            }
+
+            var invDet = 1d / det;
+
+            var tvec = ray.Orig - v0;
+
+            var u = Vector3.Dot(tvec, pvec) * invDet;
+
+            if (u < 0 || u > 1)
+            {
+                return null;
+            }
+
+            var qvec = Vector3.Cross(tvec, edge1);
+
+            var v = Vector3.Dot(ray.Dir, qvec) * invDet;
+
+            if (v < 0 || u + v > 1)
+            {
+                return null;
+            }
+
+            var t = Vector3.Dot(edge2, qvec) * invDet;
+
+            return new Vector3((float)t, (float)u, (float)v);
         }
     }
 }
