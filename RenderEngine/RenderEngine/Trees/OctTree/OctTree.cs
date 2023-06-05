9db685f1c87acc1f11a7c72b075f3d7aa433cd6b
Anatolii Parshykov <anatolii.parshykov@outlook.com>
2023-06-06 01:33:03 +0300

    OctTree constructor

---
+++ RenderEngine/RenderEngine/Trees/OctTree/OctTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RenderEngine.Interfaces;
+
+namespace RenderEngine.Trees.OctTree
+{
+    internal class OctTree
+    {
+        private const float MinSize = .0005f;
+        public BoundingBox Region { get; init; }
+        public OctTree[] Children { get; init; }
+        public List<IShape> Shapes { get; init; }
+
+        public OctTree(BoundingBox maxRegion, IEnumerable<IShape> shapes)
+        {
+            Region = maxRegion;
+            Shapes = shapes.ToList();
+            Children = new OctTree[8];
+        }
+    }
+}
