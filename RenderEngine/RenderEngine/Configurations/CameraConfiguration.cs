126253d03281acc703e9aef432d47b79dfeb2d3d
Maksym <rendon4ik@gmail.com>
2023-06-06 01:02:58 +0300

    Add configuration

---
+++ RenderEngine/RenderEngine/Configurations/CameraConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using RenderEngine.Basic;
+
+namespace RenderEngine.Configurations;
+
+public sealed record CameraConfiguration
+{
+    public static CameraConfiguration Default => new()
+    {
+        Orig = new Vector3(0, 1, 0),
+        Dir = new Vector3(0, -1, 0),
+        PixelWidth = 200,
+        PixelHeight = 200,
+        VerticalFOV = 1,
+        FocalLength = 30
+    };
+
+    public Vector3 Orig { get; init; }
+    public Vector3 Dir { get; init; }
+    public uint PixelWidth { get; init; }
+    public uint PixelHeight { get; init; }
+    public int VerticalFOV { get; init; }
+    public float FocalLength { get; init; }
+}
